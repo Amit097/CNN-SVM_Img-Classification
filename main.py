685566b85d593c9f34f71e6a003140f648e19bdb
Amit097 <44345777+Amit097@users.noreply.github.com>
2019-02-05 23:17:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 import argparse
+#from tensorflow.example.tutorials.mnist import input_data
+
+
+def parse_args():
+	parser=argparse.ArgumentParser(description='CNN-SVM for image classification')
+	gp=parser.add_argument_group('Argument')
+
+	gp.add_argument('-m','--model',required=True,type=str,help='CNN-Softmax')
+	gp.add_argument('-d','--dataset',required=True,type=str,help='path of mnist dataset')
+	
+	arguments=parser.parse_args()
+
+	return arguments
+
+
+if __name__=='__main__':
+	args=parse_args()
+
+
